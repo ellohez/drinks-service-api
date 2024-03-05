1fa09d0edea468e7cfa31b399cbd63ec811e35f1
Helen Pickavance <ellohez@gmail.com>
2024-03-05 14:28:53 +0000

    Explanation comment added

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,5 @@ import express from "express";
 export const app = express();
 // List our routes and handlers here
 // Conventially the first two parameters on each handler are req and res
+// The get method 'listens' for a specific HTTP verb on a specific path
 app.get("/", (req, res) => res.send("Welcome to the Drinks API!"));
