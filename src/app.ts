a5792ebe410c334fd414c8844ecf404360d2f798
Helen Pickavance <ellohez@gmail.com>
2024-03-05 21:12:05 +0000

    feat: added /coffee endpoint and code to pass test

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from "express";
 export const app = express();
+// Tell express to use its built in JSON parser
+app.use(express.json());
 // List our routes and handlers here
 // Conventially the first two parameters on each handler are req and res
 // The get method 'listens' for a specific HTTP verb on a specific path
 app.get("/", (req, res) => res.send("Welcome to the Drinks API!"));
 app.get("/coffeelover", (req, res) => res.send("I like coffee!"));
+app.get("/coffee", (req, res) => res.json({
+    drinkType: "Coffee",
+    name: "Latte",
+}));
+
---
+++ src/controllers/coffee_controller.test.ts
@@ -0,0 +1,12 @@
+import request from "supertest";
+import { app } from "../app";
+test("GET /coffee should return correct object", async () => {
+  // The .query syntax is specific to supertest
+  // In the real world, perhaps this would be supplie via a call to fetch in the browser??
+  const res = await request(app).get("/coffee").query({ coffeeName: "Latte" });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Latte",
+  });
+});
