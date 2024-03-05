3e23ca7a297a7ae8d1c56f396b4b620d477b9614
Helen Pickavance <ellohez@gmail.com>
2024-03-05 14:33:41 +0000

    feat: added the coffeelover endpoint and GET handler test

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,4 @@ export const app = express();
 // Conventially the first two parameters on each handler are req and res
 // The get method 'listens' for a specific HTTP verb on a specific path
 app.get("/", (req, res) => res.send("Welcome to the Drinks API!"));
+app.get("/coffeelover", (req, res) => res.send("I like coffee!"));
