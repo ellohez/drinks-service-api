fa8b0cfe8a574ad6fc00b3791eed68af53370b8d
Helen Pickavance <ellohez@gmail.com>
2024-03-07 09:43:32 +0000

    test: more edge cases tested for GET /coffeee endpoint

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -1,12 +1,39 @@
 import request from "supertest";
 import { app } from "../app";
-test("GET /coffee should return correct object", async () => {
-  // The .query syntax is specific to supertest
-  // In the real world, perhaps this would be supplied via a call to fetch in the browser??
-  const res = await request(app).get("/coffee").query({ coffeeName: "Latte" });
-  expect(res.statusCode).toEqual(200);
-  expect(res.body).toEqual({
-    drinkType: "Coffee",
-    name: "Latte",
+
+describe("GET /coffee", () => {
+  test("should return correct object with default param", async () => {
+    // The .query syntax is specific to supertest
+    // In the real world, perhaps this would be supplied via a call to fetch in the browser??
+    const res = await request(app)
+      .get("/coffee")
+      .query({ coffeeName: "Latte" });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: "Coffee",
+      name: "Latte",
+    });
+  });
+
+  test("should return correct object with a different param", async () => {
+    const res = await request(app)
+      .get("/coffee")
+      .query({ coffeeName: "Cappuccino" });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: "Coffee",
+      name: "Cappuccino",
+    });
+  });
+
+  test("should return default - latte if no coffee name is specified", async () => {
+    // No query param with coffee name specified
+    const res = await request(app).get("/coffee");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: "Coffee",
+      name: "Latte",
+    });
   });
 });
+
